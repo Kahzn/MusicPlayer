ba8d2fd179e0637dbc6270cba5b225f5a563625d
riccsPe <Come-Clarity-1993>
2017-12-07 11:50:51 +0100

    xml-Strategy wurde aktualisiert

--- src/main/java/serializable/XMLStrategy.java
+++ src/main/java/serializable/XMLStrategy.java
@@ -3,130 +3,118 @@ package serializable;
 import interfaces.Playlist;
 import interfaces.SerializableStrategy;
 import interfaces.Song;
-import model.Model;
-
 import java.beans.XMLDecoder;
 import java.beans.XMLEncoder;
 import java.io.*;
 
-
 public class XMLStrategy implements SerializableStrategy {
 
-    FileOutputStream fos = null;
-    XMLEncoder encoder = null; // = oos
+    private FileOutputStream fos = null;
+    private XMLEncoder encoder = null; // = oos
 
-    FileInputStream fis = null;
-    XMLDecoder decoder = null; // = ois
+    private FileInputStream fis = null;
+    private XMLDecoder decoder = null; // = ois
 
-    //Model model;
-    //Playlist playlist = model.getPlaylist();
-    //Playlist library = model.getLibrary();
+    private Playlist library;
 
     @Override
-    /** serialisiere library **/
+    //öffne Streams zur Serialisierung der library
     public void openWritableLibrary() throws IOException {
         fos = new FileOutputStream("xmlLibraray.xml");
         encoder = new XMLEncoder(fos);
-        System.out.println("Streams geöffnet - library - ser");
     }
 
     @Override
-    /** deserialisiere library **/
+    //öffne Streams zur Deserialisierung der library
     public void openReadableLibrary() throws IOException {
         fis = new FileInputStream("xmlLibraray.xml");
         decoder = new XMLDecoder(fis);
-        System.out.println("Streams geöffnet - library - deser");
     }
 
     @Override
-    /** serialisiere playlist **/
+    //öffne Streams zur Serialisierung der playlist
     public void openWritablePlaylist() throws IOException {
         fos = new FileOutputStream("xmlPlaylist.xml");
         encoder = new XMLEncoder(fos);
-        System.out.println("Streams geöffnet - playlist - ser");
     }
 
     @Override
-    /** deserialisiere playlist **/
+    //öffne Streams zur Deserialisierung der playlist
     public void openReadablePlaylist() throws IOException {
         fis = new FileInputStream("xmlPlaylist.xml");
         decoder = new XMLDecoder(fis);
-        System.out.println("Streams geöffnet - playlist - deser");
     }
 
     /** ******************************************************************************************************* **/
 
     @Override
-    /** schreibe Song(Objekt) **/
-    // serialisiere ein Object
+    // serialisiere ein Object (Song)
     public void writeSong(Song s) throws IOException {
-        //model.getPlaylist().add(s);
         encoder.writeObject(s);
-        System.out.println("Songs geschrieben");
     }
 
     @Override
-    /** lese Song(Objekt) **/
-    // deserialisiere ein Object
+    // deserialisiere ein Object (Song)
     public Song readSong() throws IOException, ClassNotFoundException {
-        System.out.println("Objekt geschrieben");
-        return (Song) decoder.readObject();
+        Song s;
+        s = (Song) decoder.readObject();
+        if (s != null) { return s; }
+        else { return null; }
 
     }
 
     @Override
-    /** schreibe Song(Objekt) (writeSong() ) in library **/
+    // serialisierte library (ArrayList<Song>)
+    // iteriere dazu über die library (ArrayList<Song>)
     public void writeLibrary(Playlist p) throws IOException {
-        for (Song s : p) {
-            //encoder.writeObject(s);
-            writeSong(s);
-        }
-        System.out.println("library geschrieben");
+        for (Song s : p) { writeSong(s); }
     }
 
     @Override
-    /** lese Song(Objekt) (readSong() ) aus library **/
+    // deserialisierte library (ArrayList<Song>)
+    // iteriere dazu über die library (ArrayList<Song>)
     // gibt ganze playlist zurück
-    /*
-        - erstelle neue Playlist
+    /*  - erstelle neue Playlist
         - lese Song     (exception möglich)
         - füge gelesenen Song der Playlist hinzu
-        - wenn keine Songs mehr gelesen werden können gebe Playlist zurück
-     */
+        - wenn keine Songs mehr gelesen werden können gebe Playlist zurück */
     public Playlist readLibrary() throws IOException, ClassNotFoundException {
-        //return (Playlist) decoder.readObject();
         model.Playlist p = new model.Playlist();
-        while (fis.available()!= -1) {
+        Song so = readSong(); // erstelle Objekt (einzelner ausgelesener Song)
+        while (so != null) {  // alternative Abfrage: fis.available()!= 0 (?)
             try {
-                Song s = (model.Song) decoder.readObject();
-                p.add(s);
-            } catch (Exception e) { e.printStackTrace(); }
-
+                p.add(so); //füge ausgelenes Objekt (Song) der library hinzu
+                so = readSong(); //setzte so auf das nächste auszulesene Objekt (Song) -> setzt Schleife fort
+            } catch (IOException | ArrayIndexOutOfBoundsException e) { /* e.printStackTrace(); */ break; }
         }
-        System.out.println("library ausgelesen");
+        library = p;
         return p;
     }
 
     @Override
     /** schreibe Song(Objekt) (writeSong() ) in playlist **/
     public void writePlaylist(Playlist p) throws IOException {
-        for (Song s : p) {
-            writeSong(s);
-        }
-        System.out.println("playlist geschrieben");
+        for (Song s : p) { writeSong(s); }
     }
 
     @Override
-    /** lese Song(Objekt) (readSong() ) aus playlist **/
+    //
+    //analog
     public Playlist readPlaylist() throws IOException, ClassNotFoundException {
         model.Playlist p = new model.Playlist();
-        while (fis.available()!= -1) {
+        Song so = readSong(); // erstelle Objekt (einzelner ausgelesener Song)
+        while (so != null) {  // alternative Abfrage: fis.available()!= 0 (?)
             try {
-                Song s = (model.Song) decoder.readObject();
-                p.add(s);
-            } catch (Exception e) { e.printStackTrace(); }
+                so.getId();
+                /*finde Song aus library mit dem gleichen Index wie aus ausgelesene Objekt (Song)
+                  wenn Index gleich: füge Song der Playlist p hinzu
+                     -> SELBE Songs werden in library + playlist bearbeitet ( controller.edit() ) */
+                for (Song s : library) {
+                    if(s.getId() == so.getId()) p.add(s);
+                }
+                so = readSong(); //setzte so auf das nächste auszulesene Objekt (Song) -> setzt Schleife fort
+            } catch (IOException | ArrayIndexOutOfBoundsException e) { /* e.printStackTrace(); */ break; }
         }
-        System.out.println("playlist ausgelesen");
         return p;
     }
 
@@ -136,47 +124,31 @@ public class XMLStrategy implements SerializableStrategy {
     public void closeWritableLibrary() {
         try {
             if (encoder != null) encoder.close();
-            System.out.println("encoder geschlossen");
             if (fos != null) fos.close();
-            System.out.println("fos geschlossen");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        } catch (IOException e) { e.printStackTrace(); }
     }
 
     @Override
     public void closeReadableLibrary() {
         try {
             if (decoder != null) decoder.close();
-            System.out.println("decoder geschlossen");
             if (fis != null) fis.close();
-            System.out.println("fis geschlossen");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        } catch (IOException e) { e.printStackTrace(); }
     }
 
     @Override
     public void closeWritablePlaylist() {
         try {
             if (encoder != null) encoder.close();
-            System.out.println("encoder geschlossen");
             if (fos != null) fos.close();
-            System.out.println("fos geschlossen");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        } catch (IOException e) { e.printStackTrace(); }
     }
 
     @Override
     public void closeReadablePlaylist() {
         try {
             if (decoder != null) decoder.close();
-            System.out.println("decoder geschlossen");
             if (fis != null) fis.close();
-            System.out.println("fis geschlossen");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        } catch (IOException e) { e.printStackTrace(); }
     }
 }
