872459edb1ff205828068de72320a5004f7678fd
Charlielei <rebeccamarsh@rebeccas-mbp-2.speedport.ip>
2018-01-11 15:22:52 +0100

    implementing TCPServer.java

--- src/main/java/TCP/TCPServer.java
+++ src/main/java/TCP/TCPServer.java
@@ -1,15 +1,42 @@
 package TCP;
 
-public class TCPServer extends Thread {
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
 
+public class TCPServer extends Thread {
+    private static ArrayList<String> clientNames;
 
     public TCPServer(){
+        clientNames = new ArrayList<>();
+        //int connections = 0;
+        try {
+            ServerSocket server = new ServerSocket(5020);
+            while(true){
+                Socket clientSocket = server.accept();
 
-    }
-
-    public void run(){
-
+                new TCPServerThreadForClients(clientNames, clientSocket);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
 
-//class..... thread
+
+//private class
+class TCPServerThreadForClients implements Runnable {
+    ArrayList<String> clientNames;
+    Socket socket;
+
+    public TCPServerThreadForClients(ArrayList<String> clientNames, Socket clientSocket) {
+        this.clientNames = clientNames;
+        this.socket = clientSocket;
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
