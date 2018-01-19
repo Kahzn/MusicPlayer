60417687fa6f6d288f71f4550a4539f502544c41
katha <ziolkowski@connected-organization.de>
2018-01-19 12:54:46 +0100

    UDP-Klassen gefüllt: Simple Server-Client-Kommunikation funktioniert. (Threads und Platform.runLater fehlen noch!)

--- src/main/java/UDP/UDPClient.java
+++ src/main/java/UDP/UDPClient.java
@@ -1,4 +1,57 @@
 package UDP;
 
+import java.io.IOException;
+import java.net.*;
+
 public class UDPClient {
+
+    public static void main(String[] args) {
+        // Eigene Adresse erstellen
+        InetAddress ia = null;
+        try {
+            ia = InetAddress.getByName("localhost");
+        } catch (UnknownHostException e2) {
+            e2.printStackTrace();
+        }
+        // Socket für den Klienten anlegen
+        try (DatagramSocket dSocket = new DatagramSocket(1234);) {
+
+            try {
+                int i = 0;
+                while (i < 10) {
+                    String command = "cmd:time";
+
+                    byte buffer[] = command.getBytes();
+
+                    // Paket mit der Anfrage vorbereiten
+                    DatagramPacket packet = new DatagramPacket(buffer,
+                            buffer.length, ia, 5000);
+                    // Paket versenden
+                    dSocket.send(packet);
+
+                    byte answer[] = new byte[1024];
+                    // Paket für die Antwort vorbereiten
+                    packet = new DatagramPacket(answer, answer.length);
+                    // Auf die Antwort warten
+                    dSocket.receive(packet);
+
+                    System.out.println(new String(packet.getData(),0,packet.getLength()));
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                    i++;
+                }
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+
+        }
+        catch(SocketException e){
+            e.printStackTrace();
+
+        }
+    }
 }
